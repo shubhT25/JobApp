b8a99979525b933f25ee612bd910537353ddee06
shubhT25 <shubhamtayade11235@gmail.com>
2025-02-27 09:05:25 +0530

    First Commit

---
+++ src/main/java/com/project/jobapp/controllers/JobController.java
@@ -0,0 +1,12 @@
+package com.project.jobapp.controllers;
+
+import com.project.jobapp.dto.Job;
+
+import java.util.List;
+
+public class JobController {
+    public List<Job> findAll(){
+        return null;
+    }
+
+}
